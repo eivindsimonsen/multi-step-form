e0259adaaa7b623f9ad3186ab9b9e7e14ec3b7be
EivindSimonsen <eivind.simonsen1998@gmail.com>
2023-03-17 02:10:43 +0100

    Setting up other step components

--- src/components/StepThree.jsx
+++ src/components/StepThree.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 function StepThree() {
-  return <div>StepThree</div>;
+  return (
+    <>
+      <div className="titles">
+        <h1>Personal info</h1>
+        <h2>Please provide your name, email, address, and phone number.</h2>
+      </div>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          placeholder="e.g. Stephen King"
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="e.g. stephenking@lorem.com"
+        />
+      </div>
+      <div>
+        <label htmlFor="phone">Phone Number</label>
+        <input
+          type="number"
+          id="phone"
+          placeholder="e.g. +1 234 567 890"
+        />
+      </div>
+    </>
+  );
 }
 
 export default StepThree;
--- src/components/StepTwo.jsx
+++ src/components/StepTwo.jsx
@@ -1,7 +1,38 @@
 import React from "react";
 
 function StepTwo() {
-  return <div>StepTwo</div>;
+  return (
+    <>
+      <div className="titles">
+        <h1>Select your plan</h1>
+        <h2>You have the option of monthly or yearly billing.</h2>
+      </div>
+      <div>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          placeholder="e.g. Stephen King"
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email Address</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="e.g. stephenking@lorem.com"
+        />
+      </div>
+      <div>
+        <label htmlFor="phone">Phone Number</label>
+        <input
+          type="number"
+          id="phone"
+          placeholder="e.g. +1 234 567 890"
+        />
+      </div>
+    </>
+  );
 }
 
 export default StepTwo;
